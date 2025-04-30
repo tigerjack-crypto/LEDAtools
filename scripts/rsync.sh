ddb1b1f1ce00760681cd3bcd521c8072bcdf5711
Simone Perriello <8199216+tigerjack@users.noreply.github.com>
2025-04-30 19:15:40 +0200

    ADD rsync filters and scripts

---
+++ scripts/rsync.sh
@@ -0,0 +1,28 @@
+#!/bin/bash - 
+#===============================================================================
+#
+#          FILE: rsync.sh
+# 
+#         USAGE: ./rsync.sh 
+# 
+#   DESCRIPTION: 
+# 
+#       OPTIONS: ---
+#  REQUIREMENTS: ---
+#          BUGS: ---
+#         NOTES: ---
+#        AUTHOR: YOUR NAME (), 
+#  ORGANIZATION: 
+#       CREATED: 30/04/2025 14:31
+#      REVISION:  ---
+#===============================================================================
+
+set -o nounset                              # Treat unset variables as an error
+PROJECT=LEDAtools
+PROJECT_ROOT=/mnt/data/simone/vc/crypto
+SERVER=forge
+
+rsync -avz --info=progress2 \
+  --filter="merge $PROJECT_ROOT/$PROJECT/rsync_filter.txt" \
+  "$PROJECT_ROOT/$PROJECT" "$SERVER":vc/
+
